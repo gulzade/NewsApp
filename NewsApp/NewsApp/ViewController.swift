77a6579cc4960c4dec48666dd5de232f9673fa40
gulzade <gulzadekrts@gmail.com>
2025-01-10 19:07:15 +0300

    project created

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Gülzade Karataş on 10.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
